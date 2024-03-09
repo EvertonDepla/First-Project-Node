64317c30e9cb841a2af96ad7b5774090f0c1425c
Everton Deplá <evertondutra257@gmail.com>
2024-03-09 17:33:23 -0300

    criando rota Post

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
-const port = 3000
+const uuid = require('uuid')
 
+const port = 3000
 const app = express()
 app.use(express.json())
 
-app.get('/users', (request, response) => {
-    
-    const{name, age} = request.body
-    console.log(request.body)
 
-    return response.json({ name, age})
+
+
+
+
+
+
+
+
+
+
+const users= []
+
+app.get('/users', (request, response) => {
+
+    return response.json(users)
 })
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    
+    const user = { id:uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
