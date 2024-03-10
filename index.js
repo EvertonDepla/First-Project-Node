eae377d117af8feff1fe5bdc28d3a31d4b3df3b7
Everton Deplá <evertondutra257@gmail.com>
2024-03-10 20:19:39 -0300

    adição rota put

--- index.js
+++ index.js
@@ -34,6 +34,23 @@ app.post('/users', (request, response) => {
     return response.status(201).json(user)
 })
 
+app.put('/users/:id', (request,response) => {
+    const { id } = request.params
+    const { name, age } = request.body
+
+    const updatedUser = { id, name, age }
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ message: "User not found"})
+    }
+
+    users[index] = updatedUser
+
+    return response.json(updatedUser)
+})
+
 
 
 
